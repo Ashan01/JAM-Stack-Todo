e64bbe3718aaa82299e7b55b132a72a228cf637b
Ashan Jameel <59074253+Ashan01@users.noreply.github.com>
2021-02-18 00:08:48 +0500

    commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,47 +1,46 @@
-import React, { useEffect, useState } from "react";
-import { useQuery, useMutation } from "@apollo/client";
-import gql from "graphql-tag";
+import React from "react";
+// import { useQuery,useMutation } from "@apollo/client";
+// import gql from "graphql-tag";
 
 // This query is executed at run time by Apollo.
-const GET_TODOS = gql`
-   {
-      todos {
-         task
-         id
-         status
-      }
-   }
-`;
-const ADD_TODO = gql`
-   mutation addTodo($task: String!) {
-      addTodo(task: $task) {
-         task
-      }
-   }
-`;
+// const GET_TODOS = gql`
+//    {
+//       todos {
+//          task
+//          status
+//       }
+//    }
+// `;
+// const ADD_TODO = gql`
+//    mutation addTodo($task: String!) {
+//       addTodo(task: $task) {
+//          task
+//       }
+//    }
+// `;
 
 export default function Home() {
-   let inputText;
+   // let inputText;
 
-   const [addTodo] = useMutation(ADD_TODO);
-   const addTask = () => {
-      addTodo({
-         variables: {
-            task: inputText.value,
-         },
-         refetchQueries: [{ query: GET_TODOS }],
-      });
-      inputText.value = "";
-   };
+   // const [addTodo] = useMutation(ADD_TODO);
+   // const addTask = () => {
+   //    addTodo({
+   //       variables: {
+   //          task: inputText.value,
+   //       },
+   //       refetchQueries: [{ query: GET_TODOS }],
+   //    });
+   //    inputText.value = "";
+   // };
 
-   const { loading, error, data } = useQuery(GET_TODOS);
+   // const { loading, error, data } = useQuery(GET_TODOS);
 
-   if (loading) return <h2>Loading..</h2>;
+   // if (loading) return <h2>Loading..</h2>;
 
-   if (error) {
-      console.log(error);
-      return <h2>Error</h2>;
-   }
+   // if (error) {
+   //    console.log(error);
+   //    return <h2>Error</h2>;
+   // }
 
    return (
       <div className="container">
@@ -49,12 +48,12 @@ export default function Home() {
             <h1> Add Task </h1>
             <input
                type="text"
-               ref={(node) => {
-                  inputText = node;
-               }}
+               // ref={(node) => {
+               //    inputText = node;
+               // }}
             />
          </label>
-         <button onClick={addTask}>Add Task</button>
+         {/* <button onClick={addTask}>Add Task</button> */}
          <br /> <br />
          <h3>My TODO LIST</h3>
          <table border="2">
@@ -66,8 +65,7 @@ export default function Home() {
                </tr>
             </thead>
             <tbody>
-               {data.todos.map((todo) => {
-                  console.log(todo);
+               {/* {data.todos.map((todo) => {
                   return (
                      <tr key={todo.id}>
                         <td> {todo.id} </td>
@@ -75,7 +73,7 @@ export default function Home() {
                         <td> {todo.status.toString()} </td>
                      </tr>
                   );
-               })}
+               })} */}
             </tbody>
          </table>
       </div>
