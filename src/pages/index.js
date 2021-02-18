1749bb1c8a5ebc239981c42f99531a8b737eec19
Ashan Jameel <59074253+Ashan01@users.noreply.github.com>
2021-02-18 15:58:51 +0500

    commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,16 @@ import { useQuery, useMutation } from "@apollo/client";
 import gql from "graphql-tag";
 
 export default function Home() {
-   const GET_AUTHORS = gql`{
-
-   }`;
-   const { loading, error, data } = useQuery(GET_AUTHORS);
+   const GET_TODOS = gql`
+      {
+         todos {
+            id
+            task
+            status
+         }
+      }
+   `;
+   const { loading, error, data } = useQuery(GET_TODOS);
 
    if (loading) return <h2>Loading..</h2>;
 
@@ -15,6 +21,7 @@ export default function Home() {
       return <h2>Error</h2>;
    }
 
+   console.log(data);
    return (
       <div>
          <h1>Hello World</h1>
