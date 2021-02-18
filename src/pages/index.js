15f113600db011a50dfbec055fa025e594b476b0
Ashan Jameel <59074253+Ashan01@users.noreply.github.com>
2021-02-18 15:27:36 +0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,38 +2,11 @@ import React from "react";
 import { useQuery, useMutation } from "@apollo/client";
 import gql from "graphql-tag";
 
-const GET_TODOS = gql`
-   {
-      todos {
-         task
-         id
-         status
-      }
-   }
-`;
-const ADD_TODO = gql`
-   mutation addTodo($task: String!) {
-      addTodo(task: $task) {
-         task
-      }
-   }
-`;
-
 export default function Home() {
-   let inputText;
+   const GET_AUTHORS = gql`{
 
-   const [addTodo] = useMutation(ADD_TODO);
-   const addTask = () => {
-      addTodo({
-         variables: {
-            task: inputText.value,
-         },
-         refetchQueries: [{ query: GET_TODOS }],
-      });
-      inputText.value = "";
-   };
-
-   const { loading, error, data } = useQuery(GET_TODOS);
+   }`;
+   const { loading, error, data } = useQuery(GET_AUTHORS);
 
    if (loading) return <h2>Loading..</h2>;
 
@@ -43,39 +16,8 @@ export default function Home() {
    }
 
    return (
-      <div className="container">
-         <label>
-            <h1> Add Task </h1>
-            <input
-               type="text"
-               ref={(node) => {
-                  inputText = node;
-               }}
-            />
-         </label>
-         <button onClick={addTask}>Add Task</button>
-         <br /> <br />
-         <h3>My TODO LIST</h3>
-         <table border="2">
-            <thead>
-               <tr>
-                  <th>ID</th>
-                  <th> TASK </th>
-                  <th> STATUS </th>
-               </tr>
-            </thead>
-            <tbody>
-               {data.todos.map((todo) => {
-                  return (
-                     <tr key={todo.id}>
-                        <td> {todo.id} </td>
-                        <td> {todo.task} </td>
-                        <td> {todo.status.toString()} </td>
-                     </tr>
-                  );
-               })}
-            </tbody>
-         </table>
+      <div>
+         <h1>Hello World</h1>
       </div>
    );
 }
